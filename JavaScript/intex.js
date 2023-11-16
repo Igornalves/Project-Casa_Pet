b65456dcdafa1010c0693b4636debb9021a70886
igor_alves <igornalves08@gmail.com>
2023-11-16 17:03:34 -0300

    realizando alteraçoes no projeto para que ele valide email e tambem telefones inseridos

--- JavaScript/intex.js
+++ JavaScript/intex.js
@@ -1,4 +1,4 @@
-const formulario = document.getElementById("#formulario-voluntario");
+const formulario = document.getElementById("formulario-voluntario");
 const nome = document.getElementById("nome");
 const nascimento = document.getElementById("nascimento");
 const email = document.getElementById("email");
@@ -7,15 +7,26 @@ const disponibilidade = document.getElementById("disponibilidade");
 const mensagem = document.getElementById("msg");
 const telefone = document.getElementById("telefone")
 
-// console.log(formulario,  nascimento, nome, email, disponibilidade, telefone, mensagem, formacao);
+//console.log(formulario,  nascimento, nome, email, disponibilidade, telefone, mensagem, formacao);
 
 formulario.addEventListener("submit", (event) => {
     event.preventDefault();
-
-    checkFomulario();
+    checkInputFormulario();
 })
 
-email.addEventListener("blur", () => {
+formacao.addEventListener("blur", ()=>{
+    checkInputformacao();
+})
+
+telefone.addEventListener("blur", ()=>{
+    checkInputTelefone();
+})
+
+nascimento.addEventListener("blur", ()=>{
+    checkInputNascimento();
+})
+
+email.addEventListener("blur", () =>{
     checkInputEmail();
 })
 
@@ -23,41 +34,102 @@ nome.addEventListener("blur", () => {
     checkInputNome();
 })
 
-function checkInputNome() {
-    const nomeValue = nome.value;
+function checkInputformacao(){
+    const formacaoInput = formacao.value;
 
-    if (nomeValue === "") {
-        errorInput(nome, "Preencha o seu nome !!!")
-    } else {
-        const formItem = nome.parentElement;
-        formItem.className = "input-div"
+    if (formacaoInput === "") {
+        alert("POR FAVOR DIGITE SUA FORMAÇÃO PARA QUE POSSAMOS PROSSEGUI COM O FORMULARIO")
+        return;
     }
+}
+
+function checkInputTelefone(){
+    const telefoneInput = telefone.value;
+
+    if (telefoneInput === "" || !validacaoTelefone(telefoneInput)) {
+        alert("POR FAVOR DIGITE SEU TELEFONE PARA QUE POSSAMOS PROSSEGUI COM O FORMULARIO")
+        return;
+    }
+}
+
+function validacaoTelefone(){
+    const telefoneRegex = new RegExp(
+        /^((1[1-9])|([2-9][0-9]))((3[0-9]{4}[0-9]{4})|(9[0-9]{5}[0-9]{4}))$$/
+    );
+
+    if (telefoneRegex.test(telefone)) {
+        console.log("este numero é valido pode prossegui com formulario");
+        return true;
+    }
+
+    console.log("este numero não é valido");
+    return false;
+}
+
+function checkInputNascimento(){
+    const nascimentoInput = nascimento.value;
+
+    if (nascimentoInput === "") {
+        alert("POR FAVOR DIGITE SUA DATA DE NASCIMENTO PARA QUE POSSAMOS PROSSEGUI COM O FORMULARIO")
+        return;
+    }
+}
+
+function checkInputNome() {
+    const nomeInput = nome.value;
+
+    if (nomeInput === "") {
+        alert("POR FAVOR DIGITE O EU NOME PARA QUE POSSAMOS PROSSEGUI COM O FORMULARIO")
+        return;
+    } 
+
 }
 
 function checkInputEmail() {
-    const emailValue = email.value;
+    const emailInput = email.value;
 
-    if (emailValue === "") {
-        errorInput(email, "O email é obrigatório.")
-    } else {
-        const formItem = email.parentElement;
-        formItem.className = "input-div"
-    }
+    if (emailInput === "" || !isEmailValid(emailInput)) {
+        alert("POR FAVOR DIGITE SEU EMAIL PARA QUE POSSAMOS PROSSEGUI COM O FORMULARIO")
+        return;
+    } 
 }
 
-function checkForm(){
+function isEmailValid(email){
+    const emailRegex=new RegExp(
+        /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
+    );
+
+    if (emailRegex.test(email)) {
+        // alert("EMAIL VALIDO COM SUCESSO");
+        console.log("valido com sucesso !!!")
+        return true;
+    }
+
+    // alert("VOCÊ NÃO DIGITOU O SEU EMAIL CORRETAMENTE POR FAVOR INSIRA PARA QUE POSSAMOS PROSSEGUIR");
+    console.log("não é valido este email !!! rescrever o email para que funcine e possa prosseguir")
+    return false;
+}
+
+function checkInputFormulario() {
     checkInputNome();
     checkInputEmail();
-  
+    isEmailValid();
+    checkInputNascimento();
+    checkInputTelefone();
+
     const formularioItems = formulario.querySelectorAll("#formulario-voluntario")
-  
-    const isValid = [...formularioItems].every( (item) => {
-      return item.className === "formulario-voluntario"
+
+    const isValid = [...formularioItems].every((item) => {
+        return item.className === "#formulario-voluntario"
     });
-  
-    if(isValid){
-      alert("CADASTRADO COM SUCESSO!")
+
+    if (isValid == true) {
+        alert("CADASTRADO ENVIADO COM SUCESSO!!!")
+        return;
     }
-  
-  }
-  
+
+    else if (isValid == false) {
+        alert("CADASTRO NÃO ENVIADO CONCLUIDO COM SUCESSO")
+    }
+
+}
